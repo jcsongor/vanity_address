a79e897b046b0b9b156780ad740fb90d40e03a48
jcsongor <jozsa.csongor@gmail.com>
2018-10-28 14:31:23 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bitcoin_vanity',
+      version='0.1',
+      description='Bitcoin vanity address miner',
+      url='http://github.com/jcsongor/bitcoin_vanity',
+      author='Jozsa Csongor',
+      author_email='jozsa.csongor@gmail.com',
+      license='MIT',
+      packages=['bitcoin_vanity'],
+      zip_safe=False)
