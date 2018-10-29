84c619ce931a9b887e01764ee7d25460236f3c06
jcsongor <jozsa.csongor@gmail.com>
2018-10-29 14:35:36 +0100

    implement private key generator

--- bitcoin_vanity/private_key.py
+++ bitcoin_vanity/private_key.py
@@ -11,3 +11,19 @@ class RNG(ABC):
 class SecretsRNG(ABC):
     def randbits(self, bits: int) -> int:
         return randbits(bits)
+
+
+class PrivateKeyGenerator:
+    _biggest_valid_private_key = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140
+
+    def __init__(self, rng: RNG):
+        self._rng = rng
+
+    def generate_private_key(self) -> int:
+        while True:
+            candidate = self._rng.randbits(256)
+            if self._is_valid(candidate):
+                return candidate
+
+    def _is_valid(self, private_key_candidate: int) -> bool:
+        return private_key_candidate <= self._biggest_valid_private_key
--- bitcoin_vanity/test/private_key.py
+++ bitcoin_vanity/test/private_key.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
-from unittest.mock import patch
-from bitcoin_vanity.private_key import SecretsRNG
+from unittest.mock import patch, MagicMock, call
+from bitcoin_vanity.private_key import PrivateKeyGenerator, SecretsRNG
 
 
 @patch('bitcoin_vanity.private_key.randbits')
@@ -19,3 +19,26 @@ class SecretsRNGTest(TestCase):
         result = self._rng.randbits(128)
 
         self.assertEqual(result, 12345)
+
+
+class PrivateKeyGeneratorTest(TestCase):
+    def setUp(self):
+        self._rng = MagicMock()
+        self._rng.randbits.side_effect = [
+            0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF,
+            0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141,
+            0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140,
+        ]
+
+        self._private_key_generator = PrivateKeyGenerator(self._rng)
+
+    def test_generate_private_key_generates_256_bit_integers_until_it_finds_a_valid_key(self):
+        self._private_key_generator.generate_private_key()
+
+        self._rng.randbits.assert_has_calls([call(256), call(256), call(256)])
+
+    def test_generate_private_key_returns_the_first_valid_key_it_finds(self):
+        private_key = self._private_key_generator.generate_private_key()
+
+        self.assertEqual(private_key, 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140),
+
