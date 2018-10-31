c4664a681330a2bdd904da34b4298e8712bc6dd4
jcsongor <jozsa.csongor@gmail.com>
2018-10-31 11:34:19 +0100

    add typehints

--- bitcoin_vanity/lib/hash.py
+++ bitcoin_vanity/lib/hash.py
@@ -2,9 +2,9 @@ import hashlib
 from binascii import unhexlify
 
 
-def sha256(hexstr):
+def sha256(hexstr: str) -> str:
     return hashlib.sha256(unhexlify(hexstr)).hexdigest()
 
 
-def ripemd160(hexstr):
+def ripemd160(hexstr: str) -> str:
     return hashlib.new('ripemd160', unhexlify(hexstr)).hexdigest()
