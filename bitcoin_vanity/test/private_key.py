107362684361188da97df035b465fe2b9accadbf
jcsongor <jozsa.csongor@gmail.com>
2018-10-31 11:44:14 +0100

    extract test data to fields

--- bitcoin_vanity/test/private_key.py
+++ bitcoin_vanity/test/private_key.py
@@ -24,14 +24,11 @@ class SecretsRNGTest(TestCase):
 class PrivateKeyGeneratorTest(TestCase):
     def setUp(self):
         self._rng = MagicMock()
+        self._valid_private_keys = [0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140, 1, 2, 3]
         self._rng.randbits.side_effect = [
-            0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF,
-            0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141,
-            0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140,
-            0x0000000000000000000000000000000000000000000000000000000000000001,
-            0x0000000000000000000000000000000000000000000000000000000000000002,
-            0x0000000000000000000000000000000000000000000000000000000000000003,
-        ]
+                                             0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF,
+                                             0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141,
+                                         ] + self._valid_private_keys
 
         self._private_key_generator = PrivateKeyGenerator(self._rng)
 
@@ -43,7 +40,7 @@ class PrivateKeyGeneratorTest(TestCase):
     def test_generate_private_key_returns_the_first_valid_key_it_finds(self):
         private_key = self._private_key_generator.generate_private_key()
 
-        self.assertEqual(int(private_key), 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140),
+        self.assertEqual(int(private_key), self._valid_private_keys[0]),
 
     @patch('bitcoin_vanity.private_key.PrivateKey')
     def test_generate_can_generate_compressed_and_test_network_keys(self, private_key):
@@ -53,77 +50,81 @@ class PrivateKeyGeneratorTest(TestCase):
         self._private_key_generator.generate_private_key(testnet=True, compressed=True)
 
         private_key.assert_has_calls([
-            call(0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140, testnet=False, compressed=False),
-            call(0x0000000000000000000000000000000000000000000000000000000000000001, testnet=False, compressed=True),
-            call(0x0000000000000000000000000000000000000000000000000000000000000002, testnet=True, compressed=False),
-            call(0x0000000000000000000000000000000000000000000000000000000000000003, testnet=True, compressed=True),
+            call(self._valid_private_keys[0], testnet=False, compressed=False),
+            call(self._valid_private_keys[1], testnet=False, compressed=True),
+            call(self._valid_private_keys[2], testnet=True, compressed=False),
+            call(self._valid_private_keys[3], testnet=True, compressed=True),
         ])
 
 
 class PrivateKeyTest(TestCase):
     def setUp(self):
+        """see http://gobittest.appspot.com/PrivateKey for test values"""
         self._private_key = PrivateKey(12345)
+        self._private_key_with_prefix = '800000000000000000000000000000000000000000000000000000000000003039'
+        self._testnet_private_key = 'ef0000000000000000000000000000000000000000000000000000000000003039'
+        self._private_key_with_checksum = '800000000000000000000000000000000000000000000000000000000000003039180D8C2E'
+        self._compressed_private_key = '80000000000000000000000000000000000000000000000000000000000000303901'
+        self._first_hash = '5976C6B48D1DC862AEB0C8BC3126A2751CC48E36737009DF688A9F0787A4624A'
+        self._second_hash = '180D8C2E6CBBFFCE35A0BB172CBAC966FD9AC8B7F5CB9D70C3DC8C70B90AC88C'
+        self._address = b'5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEss4BPiFsjb'
 
     def test_int_returns_the_private_key_as_an_integer(self):
         self.assertEqual(int(self._private_key), 12345)
 
     @patch('bitcoin_vanity.private_key.sha256')
     def test_bytes_hashes_the_key_twice_with_sha26(self, sha256):
-        sha256.return_value.hexdigest.return_value = '5976C6B48D1DC862AEB0C8BC3126A2751CC48E36737009DF688A9F0787A4624A'
+        sha256.return_value = self._first_hash
 
         bytes(self._private_key)
 
         sha256.assert_has_calls([
-            call(b'\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0009'),
-            call(b'Yv\xc6\xb4\x8d\x1d\xc8b\xae\xb0\xc8\xbc1&\xa2u\x1c\xc4\x8e6sp\t\xdfh\x8a\x9f\x07\x87\xa4bJ'),
-        ], True)
+            call(self._private_key_with_prefix),
+            call(self._first_hash),
+        ])
 
-    @patch('bitcoin_vanity.private_key.b58encode')
+    @patch('bitcoin_vanity.private_key.base58encode')
     @patch('bitcoin_vanity.private_key.sha256')
-    def test_bytes_base58_encodes_the_private_key_with_the_correct_checksum(self, sha256, b58encode):
-        sha256.return_value.hexdigest.return_value = '180D8C2E6CBBFFCE35A0BB172CBAC966FD9AC8B7F5CB9D70C3DC8C70B90AC88C'
-        b58encode.return_value = b'5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEss4BPiFsjb'
+    def test_bytes_base58_encodes_the_private_key_with_the_correct_checksum(self, sha256, base58encode):
+        sha256.return_value = self._second_hash
+        base58encode.return_value = self._address
 
         bytes(self._private_key)
 
-        b58encode.assert_called_once_with(b'\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0009\x18\r\x8c.')
+        base58encode.assert_called_once_with(self._private_key_with_checksum)
 
-    @patch('bitcoin_vanity.private_key.b58encode')
-    def test_bytes_returns_the_b58encoded_result(self, b58encode):
-        b58encode.return_value = b'5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEss4BPiFsjb'
+    @patch('bitcoin_vanity.private_key.base58encode')
+    def test_bytes_returns_the_base58encoded_result(self, base58encode):
+        base58encode.return_value = self._address
 
         wif = bytes(self._private_key)
 
-        self.assertEqual(wif, b'5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEss4BPiFsjb')
+        self.assertEqual(wif, self._address)
 
     def test_bytes_returns_the_private_key_in_wif_format(self):
         wif = bytes(self._private_key)
 
-        self.assertEqual(wif, b'5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEss4BPiFsjb')
+        self.assertEqual(wif, self._address)
 
     def test_str_returns_the_private_key_in_wif_format(self):
         wif = str(self._private_key)
 
-        self.assertEqual(wif, '5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEss4BPiFsjb')
+        self.assertEqual(wif, self._address.decode('utf-8'))
 
     @patch('bitcoin_vanity.private_key.sha256')
     def test_bytes_uses_the_correct_prefix_for_a_testnet_address(self, sha256):
         private_key = PrivateKey(12345, testnet=True)
-        sha256.return_value.hexdigest.return_value = '5976C6B48D1DC862AEB0C8BC3126A2751CC48E36737009DF688A9F0787A4624A'
+        sha256.return_value = self._first_hash
 
         bytes(private_key)
 
-        sha256.assert_has_calls([
-            call(b'\xef\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0009'),
-        ], True)
+        sha256.assert_any_call(self._testnet_private_key)
 
     @patch('bitcoin_vanity.private_key.sha256')
     def test_bytes_adds_the_correct_suffix_for_a_compressed_address(self, sha256):
         private_key = PrivateKey(12345, compressed=True)
-        sha256.return_value.hexdigest.return_value = '5976C6B48D1DC862AEB0C8BC3126A2751CC48E36737009DF688A9F0787A4624A'
+        sha256.return_value = self._first_hash
 
         bytes(private_key)
 
-        sha256.assert_has_calls([
-            call(b'\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0009\x01'),
-        ], True)
+        sha256.assert_any_call(self._compressed_private_key)
