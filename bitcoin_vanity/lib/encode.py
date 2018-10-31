c5d0c25f53366bb46b449b4d7fe8274416062277
jcsongor <jozsa.csongor@gmail.com>
2018-10-31 13:53:42 +0100

    code formatting

--- bitcoin_vanity/lib/encode.py
+++ bitcoin_vanity/lib/encode.py
@@ -2,8 +2,9 @@ from base58 import b58encode
 from binascii import unhexlify
 
 
-def base58encode(to_encode:str) ->bytes:
+def base58encode(to_encode: str) -> bytes:
     return b58encode(unhexlify(to_encode))
+
 
 def hex_string(number: int, length: int = 64):
     format_string = '%%0%dX' % length
