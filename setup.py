baf3ce087d78b6f79edf7a61c4ce500d0c3a074e
jcsongor <jozsa.csongor@gmail.com>
2018-11-11 16:56:32 +0100

    fix repo url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='bitcoin_vanity',
     version='0.1',
     description='Bitcoin vanity address miner',
-    url='http://github.com/jcsongor/bitcoin_vanity',
+    url='https://github.com/jcsongor/bitcoin_vanity',
     author='Jozsa Csongor',
     author_email='jozsa.csongor@gmail.com',
     license='MIT',
