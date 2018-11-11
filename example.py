a424cbc39667dc4553cc1eeb3ff2e27bcdc443d5
jcsongor <jozsa.csongor@gmail.com>
2018-11-11 21:30:20 +0100

    handle the returned tuple properly

--- example.py
+++ example.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import re
+
 from bitcoin_vanity.vanity_address_generator import VanityAddressGenerator
 
 vanity_address_generator = VanityAddressGenerator()
 pattern_to_match = re.compile('.*12[AB].*')
-vanity_address = vanity_address_generator.generate_matching_address(pattern_to_match)
-print(vanity_address)
+private_key, address = vanity_address_generator.generate_matching_address(pattern_to_match)
+print(int(private_key))
+print(address)
